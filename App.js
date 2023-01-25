698bb2759a50000b3a159a5e3e089a9bfad649cc
Aditya Kumar Singh <saaditya17@gmail.com>
2023-01-25 22:54:01 +0530

    BE  users

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {withAuthenticator} from 'aws-amplify-react-native';
-import Navigator from './src/Navigation/Navigator';
-
-
-import { Amplify } from 'aws-amplify'
-import awsconfig from './src/aws-exports'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import Navigator from "./src/Navigation/Navigator";
+import { Amplify, Auth, API, graphqlOperation } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react-native";
+import awsconfig from "./src/aws-exports";
+import { useEffect } from "react";
+import { getUser } from "./src/graphql/queries";
+import { createUser } from "./src/graphql/mutations";
 
 Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 
- function App() {
-  
-  
+function App() {
+  useEffect(() => {
+    const syncUser = async () => {
+      // get Auth user
+      const authUser = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+
+      // query the database using Auth user id (sub)
+      const userData = await API.graphql(
+        graphqlOperation(getUser, { id: authUser.attributes.sub })
+      );
+
+      if (userData.data.getUser) {
+        console.log("User already exists in DB");
+        return;
+      }
+      // if there is no users in db, create one
+      const newUser = {
+        id: authUser.attributes.sub,
+        name: authUser.attributes.phone_number,
+        status: "Hey, I am using WhatsApp",
+      };
+
+      await API.graphql(
+        graphqlOperation(createUser, { input: newUser })
+      );
+    };
+
+    syncUser();
+  }, []);
+
   return (
     <View style={styles.container}>
-     <Navigator/>
+      <Navigator />
+
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -22,8 +54,9 @@ Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'whitesmoke',
-    justifyContent: 'center',
+    backgroundColor: "whitesmoke",
+    justifyContent: "center",
   },
 });
-export default  withAuthenticator(App);
+
+export default withAuthenticator(App);
